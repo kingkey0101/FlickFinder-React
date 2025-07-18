b750eb1d73f18c3b7525bf6555318635368bd438
Keylan King <kingkey0101@outlook.com>
2025-07-18 18:40:28 -0500

    adding logic to Home.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Searchbar = () => {
   return (
     <div className="search__bar">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Trending from "../components/Trending";
 import Spinner from "../components/Spinner";
 
-const API_KEY = "YOUR_KEY";
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+const BASE_URL = process.env.REACT_APP_TMDB_BASE_URL;
 
 const Home = () => {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchMovies = async (searchTerm) => {
+    setLoading(true);
+    let endpoint = searchTerm
+      ? `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+          searchTerm
+        )}`
+      : `${BASE_URL}/movie/popular?api_key=${API_KEY}`;
+    try {
+      const res = await fetch(endpoint);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchMovies("");
+  }, []);
+  useEffect(() => {
+    if (query !== "") fetchMovies(query);
+  }, [query]);
   return (
     <>
-      <Header />
+      <Header setQuery={setQuery} />
       <Hero />
-      <Trending />
+      {loading ? <Spinner /> : <Trending movies={movies}/>}
     </>
   );
 };
